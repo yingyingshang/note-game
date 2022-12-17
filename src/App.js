5c8becd4e677e6df7035935c3657515430130150
Brita Shang <brita.shang@activenetwork.com>
2022-12-17 14:43:17 +0800

    add notes app

--- src/App.js
+++ src/App.js
@@ -3,24 +3,83 @@ import './App.css';
 import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import Sidebar from './components/Sidebar';
+import Editor from './components/Editor';
+import {data} from "/.data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
 
-function App() {
-  const [darkMode, setDarkMode] = React.useState(true)
+export default function App() {
 
-  function toggleDarkMode(){
-    setDarkMode(prevMode => !prevMode)
-  }
+    const [notes, setNotes] = React.useState(
+      JSON.parse(localStorage.getItem("notes")) || []
+    )
 
-  return(
-    <div className="container">
-      <Navbar 
-        darkMode={darkMode}
-        toggleDarkMode={toggleDarkMode}
-      />
-      <Main darkMode={darkMode} />
-    </div>
-  )
-  
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+      (notes[0] && note[0].id) || ""
+    )
+
+    React.useEffect(()=>{
+      localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+
+    function createNewNote(){
+      const newNote = {
+        id: nanoid(),
+        body: "# Type your markdown note's title here"
+      }
+      setNotes(preNotes => [newNote, ...preNotes])
+      setCurrentNoteId(newNote.id)
+    }
+
+    function updateNote(text){
+      setNotes(oldNotes => oldNotes.map(oldNote => {
+        return oldNote.id === currentNoteId
+          ? {...oldNote, body: text} 
+          : oldNote
+      }))
+    }
+
+    function findCurrentNote() {
+      return notes.find(note => {
+        return note.id === currentNoteId
+      }) || notes[0]
+    }
+
+    const [darkMode, setDarkMode] = React.useState(true)
+
+    function toggleDarkMode(){
+      setDarkMode(prevMode => !prevMode)
+    }
+
+    return (
+      <main>
+        {
+          notes.length > 0 
+          ?
+          <Split sizes={[30, 70]} direction="horizontal" className="split">
+            <Sidebar notes={notes} currentNote={findCurrentNote()} setCurrentNoteId={setCurrentNoteId} newNote={createnewNote} />
+            {currentNoteId && notes.length > 0 && <Editor currentNote={findCurrentNote()} updateNote={updateNote} />}
+          </Split>
+          : 
+          <div className='no-notes'>
+            <h1>You have no notes</h1>
+            <button className="first-note" onClick={createNewNote}>
+              Create one now
+            </button>
+          </div>
+        }
+        <div className="container">
+        <Navbar 
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+        <Main darkMode={darkMode} />
+      </div>
+      </main>
+      
+    )
+    
   // return (
   //   <div className="App">
   //     <header className="App-header">
@@ -41,4 +100,4 @@ function App() {
   // );
 }
 
-export default App;
+
